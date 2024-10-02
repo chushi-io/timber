3b0df8465ecd36e3b791f15a9ad2921aa07e6ed7
Robert Wittman <robkwittman@gmail.com>
2024-10-01 20:24:21 -0400

    Add debug logs

--- adapter/adapter.go
+++ adapter/adapter.go
@@ -3,6 +3,7 @@ package adapter
 import (
 	"connectrpc.com/connect"
 	"context"
+	"fmt"
 	v1 "github.com/chushi-io/timber/gen/server/v1"
 	"github.com/chushi-io/timber/gen/server/v1/serverv1connect"
 	"github.com/chushi-io/timber/interceptor"
@@ -11,31 +12,39 @@ import (
 )
 
 type Adapter struct {
-	stream *connect.ClientStreamForClient[v1.StreamLogsRequest, v1.StreamLogsResponse]
-	source string
+	logService serverv1connect.LogsServiceClient
+	stream     *connect.ClientStreamForClient[v1.StreamLogsRequest, v1.StreamLogsResponse]
+	source     string
 }
 
 func New(address string, authToken string, resource string) io.Writer {
 	interceptors := connect.WithInterceptors(
 		interceptor.NewClientAuthInterceptor(authToken),
 	)
-	logService := serverv1connect.NewLogsServiceClient(
-		http.DefaultClient,
-		address,
-		interceptors,
-	)
 	return &Adapter{
-		stream: logService.Forward(context.TODO()),
+		logService: serverv1connect.NewLogsServiceClient(
+			http.DefaultClient,
+			address,
+			interceptors,
+		),
 	}
 }
 
 func (a *Adapter) Write(p []byte) (int, error) {
+	if a.stream == nil {
+		fmt.Println("opening stream")
+		a.stream = a.logService.Forward(context.TODO())
+	}
+	fmt.Println("sending log message")
+	fmt.Println(string(p))
 	if err := a.stream.Send(&v1.StreamLogsRequest{
 		Resource: a.source,
 		Logs:     p,
 	}); err != nil {
+		fmt.Println(err)
 		return 0, err
 	}
+	fmt.Println("log message sent")
 	return len(p), nil
 }
 
